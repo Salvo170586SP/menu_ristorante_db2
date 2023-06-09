ccadc38a6f29aa438e8b3345f261cb2609046f92
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2023-06-09 18:17:02 +0200

    added migration aperitifs

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AperitifController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,3 +30,9 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
+
+    //Aperitivi
+    Route::resource('/aperitifs',  AperitifController::class);
+});
